d2050b6d5864e05649c335f174a2882a61f6e14c
Murphy2305 <kpshekhawat678@gmail.com>
2023-08-19 12:27:03 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const adviceno = document.getElementById("adviceno");
+const advice = document.getElementById("advice");
+const button = document.querySelector(".btn");
+
+async function generate() {
+  const res = await fetch("https://api.adviceslip.com/advice")
+    .then((data) => {
+      return data.json();
+    })
+    .then((data) => {
+      adviceno.innerText = "Advice # " + data.slip.id;
+      advice.innerText = '"' + data.slip.advice + '"';
+    });
+}
+
+button.addEventListener("click", generate);
